5a179243ee3b76dda4d9022f2d013e457727f9aa
Pedro Falco <fpfalco@gmail.com>
2024-09-22 00:23:38 -0300

    chore: actualice el collage del cta

--- src/componentes/CTA.svelte
+++ src/componentes/CTA.svelte
@@ -1,5 +1,5 @@
 <div class="p-7">
-    <div class="relative text-white h-[750px] bg-cover bg-no-repeat bg-center flex justify-center items-center" style="background-image: url(carousel/collage2.png);">
+    <div class="relative text-white h-[750px] bg-cover bg-no-repeat bg-center flex justify-center items-center" style="background-image: url(/carousel/collage3.png);">
         <div class="absolute inset-0 bg-black opacity-45"></div> <!-- This darkens the background -->
         <div class="relative text-center space-y-4">
             <h1 class="text-4xl md:text-7xl">Tus ideas en impresiones</h1>
